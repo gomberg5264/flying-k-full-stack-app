10c48f493f33bbb0ac4e4251cb2e36a7611a2322
Travis Ricks <travis@travisricks.com>
2020-02-07 15:15:45 +0900

    fix migrations

--- migrations/20200207122950_all_data_locations_table.js
+++ migrations/20200207122950_all_data_locations_table.js
@@ -1,25 +1,5 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("locations", (table) => {
-    table.increments().index();
-
-    table.float("latitude");
-
-    table.float("longitude");
-
-    table.text("name").notNullable();
-
-    table.float("number").notNullable();
-
-    table.text("prefName");
-
-    table.text("cityName");
-
-    table.text("stateName");
-
-    table.text("highway");
-
-    table.text("restaurant");
-
+  return knex.schema.alterTable("locations", (table) => {
     table.text("type");
 
     table.specificType("amenities", "text ARRAY");
